def87598cc09a44a1e23bff901d098bd4519a22e
abhinav <abhinavrajesh2002@gmail.com>
2024-04-19 20:19:18 +0530

    seo

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Abhinav O',
-  description: 'Portfolio of Abhinav O',
+  description: 'Abhinav O - Web Developer Portfolio** | Crafted with Next.js Explore the immersive portfolio of Abhinav O, an accomplished web developer and tech enthusiast. Witness stunning web designs, innovative projects , and discover how Abhinav passion for technology translates into impactful solutions.',
 }
 
 export default function RootLayout({ children }) {
