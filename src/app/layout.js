81cbe420438fb50293306e50f688526600ca31de
abhinav <abhinavrajesh2002@gmail.com>
2024-12-27 22:51:07 +0530

    verification meta tag for google search console

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,12 +7,13 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Abhinav O',
-  description: 'Abhinav O - Web Developer Portfolio** | Crafted with Next.js Explore the immersive portfolio of Abhinav O, an accomplished web developer and tech enthusiast. Witness stunning web designs, innovative projects , and discover how Abhinav passion for technology translates into impactful solutions.',
+  description: 'Abhinav O - Web Developer Portfolio** |  Portfolio of Abhinav O, an accomplished web developer and tech enthusiast. Witness stunning web designs, innovative projects , and discover how Abhinav passion for technology translates into impactful solutions.',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <meta name="google-site-verification" content="fmt08mX7H4SGXS1NXZ4TF0JgF7M0awTax-1stcm58g4" />
       <body className={inter.className}>{children}
       <SpeedInsights/>
       <Analytics/>
